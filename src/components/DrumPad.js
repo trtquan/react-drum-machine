2eee2b98d669f7cab2f1c8a8c8ae15e91172be0c
quantran <trtquan@gmail.com>
2020-09-03 10:54:19 +0700

    drumpad compo

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
 
 export default class DrumPad extends Component {
   constructor(props) {
